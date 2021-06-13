370742c1ee5aefb6876a430b721ea406dd414959
Colin Tseng <klt.aus@yahoo.com>
2021-06-13 18:42:31 +1000

    Add more images

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -7,6 +7,11 @@ const BackgroundImage = [
     "https://www.artic.edu/iiif/2/bbbedb77-ff47-4c12-d1c2-e94499bb6ae2/full/843,/0/default.jpg",
     "https://www.artic.edu/iiif/2/aab879bf-4a41-7984-101a-61d6712d333c/full/843,/0/default.jpg",
     "https://artic-web.imgix.net/4d01f586-3fa6-4f83-b93f-6eb5db4feddd/exh_supernatural-shakespeare_mega_2000.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=%2C%2C%2C",
+    "https://www.artic.edu/iiif/2/9d1e8a40-d5d7-c4e9-4bc5-d4dea8ab31e3/full/843,/0/default.jpg",
+    "https://www.artic.edu/iiif/2/b272df73-a965-ac37-4172-be4e99483637/full/843,/0/default.jpg",
+    "https://artic-web.imgix.net/c9d39c47-9ced-4cd4-a1ef-84edf9fe67ee/2002.378BaglioneGiovanni.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=0%2C89%2C1689%2C952",
+    "https://artic-web.imgix.net/4d01f586-3fa6-4f83-b93f-6eb5db4feddd/exh_supernatural-shakespeare_mega_2000.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=%2C%2C%2C",
+    "https://artic-web.imgix.net/1a2d48f8-6e8e-4708-97e4-af62d68b5db9/cal-LostChicago_05242018.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=11%2C15%2C466%2C263"
     
 ]
 
